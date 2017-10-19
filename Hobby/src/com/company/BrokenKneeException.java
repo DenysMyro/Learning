6f668de37ee26592afb302bc9d598c93a2ea642c
Denys Myro <myroshnychenko.denys@gmail.com>
2017-10-19 12:21:11 +0300

    New exception was added

--- Hobby/src/com/company/BrokenKneeException.java
+++ Hobby/src/com/company/BrokenKneeException.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class BrokenKneeException extends Throwable {
 
-    String message = "Your knee has been broken";
+    private static final String MESSAGE ="To do a perfect jump your speed must be between 15 and 20";
+
+
+    public BrokenKneeException(int speed) {
+        super("Your knee has been broken because of crash on speed: "+speed+". "+MESSAGE);
+    }
 
 }
--- Hobby/src/com/company/Main.java
+++ Hobby/src/com/company/Main.java
@@ -5,21 +5,24 @@ public class Main {
     public static void main(String[] args) {
 
         Hobby noHobby = new Hobby();
-        MountingBiking Biking = new MountingBiking(2, "Commencal");
-        Hobby MTB = new MountingBiking("el Camino");
+        MountingBiking biking = new MountingBiking(2, "Commencal");
+        Hobby mtb = new MountingBiking("el Camino");
+        VideoGaming gaming = new VideoGaming(3,"Dark Souls");
 
-        Hobby list [] = {noHobby, Biking, MTB};
+        Hobby list [] = {noHobby, biking, mtb, gaming};
 
         for (Hobby hobby : list) {
             hobby.tellAboutHobby();
         }
 
-        try {
-            Biking.doCrazyJump();
-        } catch (BrokenKneeException e){
-            e.getMessage();
-            System.out.println("exception caught");
+        try{
+            biking.doCrazyJumpOnSpeed(25);
+        } catch (LowSpeedException e) {
+            System.out.println("exception 1 caught "+"\n"+ e.getMessage());
+        } catch (BrokenKneeException e) {
+            System.out.println("exception 2 caught "+"\n"+ e.getMessage());
         }
 
     }
+
 }
