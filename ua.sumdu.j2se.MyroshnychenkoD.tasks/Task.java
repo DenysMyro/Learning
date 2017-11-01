ec526d9cb23dbd09ab1f0416ca86999f738bb357
Denys Myroshnychenko <32217521+DenysMyro@users.noreply.github.com>
2017-11-01 23:30:47 +0200

    Add files via upload
    update

--- ua.sumdu.j2se.MyroshnychenkoD.tasks/Task.java
+++ ua.sumdu.j2se.MyroshnychenkoD.tasks/Task.java
@@ -1,91 +1,104 @@
+package ua.edu.sumdu.j2se.MyroshnychenkoDenys.tasks;
+
 public class Task {
 
-	String taskTitle;
-	int taskTime;
-	int taskStartTime;
-	int taskEndTime;
-	int taskTimeInterval;
-	boolean isRepeatable;
-	boolean isActibe;
+    String taskTitle;
+    int taskTime;
+    int taskStartTime;
+    int taskEndTime;
+    int taskTimeInterval;
+    boolean isRepeatable;
+    boolean isActive;
 
 	/*Constructors*/
 
-	public Task (String title, int time) {
-		this.taskTitle = title;
-		this.taskTime = time;
-		isAtive = false;
-		isRepeatable = false;
-	}
+    public Task (String title, int time) {
+        this.taskTitle = title;
+        this.taskTime = time;
+        isActive = false;
+        isRepeatable = false;
+    }
 
-	public Task (String title, int start, int end, int interval) {
-		this.tasktitle = title;
-		this.taskStartTime = start;
-		this.taskEndTime = end;
-		this.taskTimeInterval = interval;
-		isActive = false;
-		isRepeatable = true;
-		
-	}
+    public Task (String title, int start, int end, int interval) {
+        this.taskTitle = title;
+        this.taskStartTime = start;
+        this.taskEndTime = end;
+        this.taskTimeInterval = interval;
+        isActive = false;
+        isRepeatable = true;
+
+    }
 	
 	/*Getters&Setters*/
 
-	public String getTitle () { return taskTitle; }
-	
-	public void setTitle (String title) { this.taskTitle = title; }
-	
-	public boolean getIsActive () { return isActive; }
-	
-	public void setActive (boolean state) { this.isActive = state; }
+    public String getTitle () { return taskTitle; }
 
-	public int getTime  () { 
-		if(isRepetable) {
-		return taskStartTime;
-		} else {
-		return taskTime;
-		}
-	}
+    public void setTitle (String title) { this.taskTitle = title; }
 
-	public void setTime (int time) { 
-		taskStartTime = time; 
-		isRepeatable = false;
-	}
+    public boolean isActive () { return isActive; }
 
-	public int getStartTime () {
-		if(isRepetable) {
-		return taskStartTime;
-		} else {
-		return taskTime;
-		}
-	}
-	
-	public int getEndTime () {
-		if(isRepetable) {
-		return taskEndTime;
-		} else {
-		return taskTime;
-		}
-	}
+    public void setActive (boolean state) { this.isActive = state; }
 
-	public int getRepeatInterval () {
-	if(isRepetable) {
-		return taskTimeInterval;
-		} else {
-		return 0;
-		}
-	}
-	
-	
-	public void setTime (int start, int end, int interval) {
-		taskStartTime = start;
-		taskEndTime = end;
-		taskTimeInterval = interval;
-		isRepeatable = true;
-	}
+    public int getTime  () {
+        if(isRepeatable) {
+            return taskStartTime;
+        } else {
+            return taskTime;
+        }
+    }
 
-	public boolean isRepeated () {
-		retun isRepeatable;	
-	}
+    public void setTime (int time) {
+        taskTime = time;
+        isRepeatable = false;
+    }
+
+    public int getStartTime () {
+        if(isRepeatable) {
+            return taskStartTime;
+        } else {
+            return taskTime;
+        }
+    }
+
+    public int getEndTime () {
+        if(isRepeatable) {
+            return taskEndTime;
+        } else {
+            return taskTime;
+        }
+    }
+
+    public int getRepeatInterval () {
+        if(isRepeatable) {
+            return taskTimeInterval;
+        } else {
+            return 0;
+        }
+    }
 
 
+    public void setTime (int start, int end, int interval) {
+        taskStartTime = start;
+        taskEndTime = end;
+        taskTimeInterval = interval;
+        isRepeatable = true;
+    }
 
+    public boolean isRepeated () {
+        return isRepeatable;
+    }
+    
+    public int nextTimeAfter (int current) {
+        if (isRepeatable) {
+          return taskStartTime += taskTimeInterval;
+       }else {
+       if (isActive && current<taskTime) {
+            return taskTime;
+          } else {
+          return -1;
+          }
+        }   
+    }
+    
 }
+
