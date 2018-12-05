be117ac0afd5cdaf802c49147262b033adf77e5a
Denys Myro <myroshnychenko.denys@gmail.com>
2018-12-05 12:36:53 +0200

    Lib Added

--- TimeReportsComparator/src/com/company/Main.java
+++ TimeReportsComparator/src/com/company/Main.java
@@ -1,29 +1,40 @@
 package com.company;
 
 
+import java.io.File;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        String csvFile = "C:\\IdeaProjects\\Learning\\TimeReportsComparator\\October.csv";
+//        File doc = new File("C:\\IdeaProjects\\Learning\\TimeReportsComparator\\October.xls");
+        File doc = new File("/home/denys/IdeaProjects/Learning/TimeReportsComparator/October.xls");
 
-        TimeTrackingSys sys1 = new TimeTrackingSys("NAV");
-        sys1.addPerson("Radko Roman", "ASI Android", 100.0);
-        sys1.addPerson("Serg Martynenko", "DCBFC", 8.0);
-        sys1.addPerson("Denka Myro", "Pacify", 40.0);
-        sys1.addPerson("Denka Myro", "Medela", 20.0);
-        sys1.addPerson("Denka Myro", "Medela", 20.0);
-        sys1.addPerson("Denkas Myro", "Medela", 20.0);
+        ParcerOne parse = new ParcerOne();
 
-        TimeTrackingSys sys2 = new TimeTrackingSys("QuickBooks");
-        sys2.addPerson("Radko Roman", "ASI Android", 150.0);
-        sys2.addPerson("Radko Roman", "LoompingTheDay", 1.0);
-        sys2.addPerson("Serg Martynenko", "DCBFC", 8.0);
-        sys2.addPerson("Serg Martynenko", "DCBFC", 8.0);
-        sys2.addPerson("Denka Myro", "Pacify", 40.0);
-        sys2.addPerson("Denka Myro", "Medela", 20.0);
+        parse.setExcelFile(doc,"Sheet1");
+        System.out.println(parse.getCellData(4,1));
 
-        System.out.println(sys1.compare(sys2));
+
+
+
+//        TimeTrackingSys sys1 = new TimeTrackingSys("NAV");
+//        sys1.addPerson("Radko Roman", "ASI Android", 100.0);
+//        sys1.addPerson("Serg Martynenko", "DCBFC", 8.0);
+//        sys1.addPerson("Denka Myro", "Pacify", 40.0);
+//        sys1.addPerson("Denka Myro", "Medela", 20.0);
+//        sys1.addPerson("Denka Myro", "Medela", 20.0);
+//        sys1.addPerson("Denkas Myro", "Medela", 20.0);
+//
+//        TimeTrackingSys sys2 = new TimeTrackingSys("QuickBooks");
+//        sys2.addPerson("Radko Roman", "ASI Android", 150.0);
+//        sys2.addPerson("Radko Roman", "LoompingTheDay", 1.0);
+//        sys2.addPerson("Serg Martynenko", "DCBFC", 8.0);
+//        sys2.addPerson("Serg Martynenko", "DCBFC", 8.0);
+//        sys2.addPerson("Denka Myro", "Pacify", 40.0);
+//        sys2.addPerson("Denka Myro", "Medela", 20.0);
+
+//        System.out.println(sys1.compare(sys2));
 
     }
 }
--- TimeReportsComparator/src/com/company/ParcerOne.java
+++ TimeReportsComparator/src/com/company/ParcerOne.java
@@ -1,8 +1,64 @@
 package com.company;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.*;
 
 public class ParcerOne {
 
+    private static XSSFSheet ExcelWSheet;
+    private static XSSFWorkbook ExcelWBook;
+    private static XSSFCell Cell;
+    private static XSSFRow Row;
+    File fileName = new File("home/denys/IdeaProjects/Learning/TimeReportsComparator/Fund.xlsx");
+
+
+    public void setExcelFile(File Path, String SheetName) throws Exception {
+
+        try {
+            FileInputStream ExcelFile = new FileInputStream(Path);
+            ExcelWBook = new XSSFWorkbook(ExcelFile);
+            ExcelWSheet = ExcelWBook.getSheet(SheetName);
+        } catch (Exception e) {
+            throw (e);
+        }
+
+    }
+
+    public  String getCellData(int RowNum, int ColNum) throws Exception {
+
+        try {
+            Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+            String CellData = Cell.getStringCellValue();
+            return CellData;
+        } catch (Exception e) {
+
+            return "";
+
+        }
+
+    }
+
+    public static void setCellData(String Result, int RowNum, int ColNum, File Path) throws Exception {
+
+        try {
+            Row = ExcelWSheet.createRow(RowNum - 1);
+            Cell = Row.createCell(ColNum - 1);
+            Cell.setCellValue(Result);
+            FileOutputStream fileOut = new FileOutputStream(Path);
+            ExcelWBook.write(fileOut);
+            fileOut.flush();
+            fileOut.close();
+        } catch (Exception e) {
+
+            throw (e);
+
+        }
+
+    }
 
 }
+
